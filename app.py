d7d623e8ada151cee5fe97785a69102d7680a226
Rahul-web087 <nayakrahul9028@gmail.com>
2025-11-01 13:18:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+# import pandas as pd
+# import numpy as np
+st.title("Welcome to Rn Education Odia Youtube Channel")
+
+
+st.header("What we provide in this Channel")
+st.markdown("We are providing you All Latest Educational update")
+st.markdown("We are providing you All Latest Scholarship update")
